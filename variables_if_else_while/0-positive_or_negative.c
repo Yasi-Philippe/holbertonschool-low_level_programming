75c4a2141e85fafa1b6cd2ab273851dd2144875c
Yasi-Philippe <shyasi95@gmail.com>
2025-10-21 16:03:39 +0200

    feat: Script that evaluates if a number is positive or negative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Will return whether the var n is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+		puts("0 is zero");
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is positive\n", n);
+
+	return (0);
+}
