e2c855035618e52eb3bcbb9ebb1b19a43228d457
Yasi-Philippe <shyasi95@gmail.com>
2025-10-24 10:24:22 +0200

    fix: Improved coding style to match with Betty coding style

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints 10 times the alphabet
- *
+ * _islower - checks whether the character is lowercase or not
+ * @c: Input character tu run the test in
  * Return: 1 if c is lowercase, 0 if it's not.
  */
 int _islower(int c)
