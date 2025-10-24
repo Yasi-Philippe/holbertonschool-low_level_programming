708b90ee1b296f4f08461959df1d2dfc84083c2e
Yasi-Philippe <shyasi95@gmail.com>
2025-10-24 14:50:20 +0200

    feat: C function that prints all minutes in an hour

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - Conputes every single hour
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 23; a++)
+	{
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar('0' + a / 10);
+			_putchar('0' + a % 10);
+			_putchar(':');
+			_putchar('0' + b / 10);
+			_putchar('0' + b % 10);
+			_putchar('\n');
+		}
+	}
+}
