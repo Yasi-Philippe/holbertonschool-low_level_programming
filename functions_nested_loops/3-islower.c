ae4169cb1e0627e3f97d18eeef492d9afb5a02d5
Yasi-Philippe <shyasi95@gmail.com>
2025-10-24 10:20:38 +0200

    feat: C Function to detect whether a character is lowercase or not

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: 1 if c is lowercase, 0 if it's not.
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	return (0);
+}
