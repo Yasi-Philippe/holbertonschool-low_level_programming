e9d1ce07d9832a5c3a25a185be3b9310ce43937e
Yasi-Philippe <shyasi95@gmail.com>
2025-10-24 11:43:35 +0200

    feat: C function that returns the absolute value of a number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute number of a number
+ * @n: Integer to make the test in.
+ * Return: n if the number is positive. -n if the number is negative
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	return (-n);
+}
