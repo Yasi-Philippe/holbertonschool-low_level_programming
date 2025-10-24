f0f2228c59527cd57e0df399606cad1aaba64c23
Yasi-Philippe <shyasi95@gmail.com>
2025-10-24 11:12:23 +0200

    fix: Improved coding style to match Betty Coding Style

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - checks the sign of a number
- * @c: Input integer tu run the test in
+ * @n: Input integer tu run the test in
  * Return: +, 1 if c is a positive number, 0, 0.
  */
 int print_sign(int n)
@@ -16,6 +16,6 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
-	_putchar('0');	
+	_putchar('0');
 	return (0);
 }
