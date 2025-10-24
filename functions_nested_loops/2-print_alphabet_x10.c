5a739a760f1e7472d4ca7de6ab074770cb8bc8f0
Yasi-Philippe <shyasi95@gmail.com>
2025-10-24 09:45:09 +0200

    feat: C Script with a function that prints 10 times the alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: 0.
+ */
+int print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 1; c <= 10; c++)
+		print_alphabet();
+	return (0);
+}
