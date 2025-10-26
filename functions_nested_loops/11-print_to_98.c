d2b6cbcf55df39d0d5c52e8ee714a606d0bab005
Yasi-Philippe <shyasi95@gmail.com>
2025-10-26 16:03:10 +0100

    feat: C function that lists all integer numbers to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all integer numbers until 98
+ * @n: Integer input
+ *
+ * Return: Void
+ */
+void print_to_98(int n)
+{
+	int a;
+
+	if (n <= 98)
+		for (a = n; a <= 98; a++)
+			printf("%d, ", a);
+	else
+		for (a = n; a >= 98; a--)
+			printf("%d, ", a);
+	putchar('\n');
+}
