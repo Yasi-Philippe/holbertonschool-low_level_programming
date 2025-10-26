82a043ddc472b285482acd101da7e2af78a8c0a4
Yasi-Philippe <shyasi95@gmail.com>
2025-10-26 16:07:15 +0100

    fix: Removed the last comma of every line

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,9 +13,17 @@ void print_to_98(int n)
 
 	if (n <= 98)
 		for (a = n; a <= 98; a++)
-			printf("%d, ", a);
+		{
+			printf("%d", a);
+			if (a != 98)
+				printf (", ");
+		}
 	else
 		for (a = n; a >= 98; a--)
-			printf("%d, ", a);
+		{
+			printf("%d", a);
+			if (a != 98)
+				printf (", ");
+		}
 	putchar('\n');
 }
