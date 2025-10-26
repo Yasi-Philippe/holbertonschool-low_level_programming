875ef9db969250388752a58d5b42afc557990c52
Yasi-Philippe <shyasi95@gmail.com>
2025-10-26 19:35:17 +0100

    fix: Revamped script to not include specific commands

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 /**
 * main - Returns the size of variables
@@ -8,14 +9,13 @@
 
 int main(void)
 {
-	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n";
 	size_t i = 0;
 
 	while (str[i] != '\0')
 	{
-		putchar(str[i]);
+		write(1, &str[i], 1);
 		i++;
 	}
-	putchar('\n');
 	return (1);
 }
