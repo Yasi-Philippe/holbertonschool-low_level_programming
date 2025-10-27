0e507be1a807c0a682d2af96744d7f18fe405b52
Yasi-Philippe <shyasi95@gmail.com>
2025-10-27 09:48:59 +0100

    feat: C function that prints all the digits

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints all the numbers.
+ *
+ *
+ * Return: Void.
+ */
+void print_numbers(void)
+{
+	char a;
+
+	for (a = 48; a <= 57; a++)
+		_putchar(a);
+	_putchar('\n');
+}
