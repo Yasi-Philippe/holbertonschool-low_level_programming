27e17942f7aabea4de5ca0ce2de26020368dc68a
Yasi-Philippe <shyasi95@gmail.com>
2025-10-27 13:04:46 +0100

    feat: C script that prints a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_traingle - Prints a traingle
+ * @size: Input integer.
+ *
+ * Return: Void.
+ */
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size > 0)
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = 1; b <= (size - a); b++)
+				_putchar(' ');
+			for (c = 1; c <= a; c++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
