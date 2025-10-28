4ab5a2ea415e6a3394b0507a8235a03682a1a3e2
Yasi-Philippe <shyasi95@gmail.com>
2025-10-28 09:21:40 +0100

    feat: C function that prints a string reversed

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string reversed followed by a new line.
+ * @s: String to print.
+ *
+ * Return: Void.
+ */
+void print_rev(char *s)
+{
+	int a;
+
+	for (a = 0; *s != '\0'; s++)
+		a++;
+	for (; a >= 0; s--)
+	{
+		_putchar(*s);
+		a--;
+	}
+	_putchar('\n');
+}
