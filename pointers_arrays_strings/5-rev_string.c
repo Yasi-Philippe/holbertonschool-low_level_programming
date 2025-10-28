f5b757ea5f2762b387877505f741343e310b8ac1
Yasi-Philippe <shyasi95@gmail.com>
2025-10-28 14:21:08 +0100

    feat: C script that reverses a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - Returns a string reversed.
+ * @s: String to reverse.
+ *
+ * Return: Void.
+ */
+void rev_string(char *s)
+{
+	int a = 0, l;
+	char temp[1000];
+
+	while (s[a] != '\0')
+	{
+		temp[a] = s[a];
+		a++;
+	}
+	l = a -1;
+	for (a = 0; a <= l; a++)
+	{
+		s[a] = temp[l-a];
+	}
+}
