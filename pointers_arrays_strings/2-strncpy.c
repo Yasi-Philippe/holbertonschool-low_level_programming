db8243ca44dfb3354b7c859a544a5ad70e3b825d
Yasi-Philippe <shyasi95@gmail.com>
2025-10-29 22:15:12 +0100

    fix: Added a condition for empty strings

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,8 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int c = 1;
 
-	if (n <= 0)
+	if (n <= 0 || *src == '\0')
 		return (dest);
-	if (*src == '\0')
-	{
-		*dest = '\0';
-		return (dest);
-	}
 	while ((*dest++ = *src++))
 	{
 		if (c == n)
