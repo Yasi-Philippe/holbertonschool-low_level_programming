223d1da77874450b7846028d0e0056b92e99dcf8
Yasi-Philippe <shyasi95@gmail.com>
2025-10-29 10:53:01 +0100

    feat: C Function that prints part of an array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Prints a defined number of array elements.
+ * @a: Input int array.
+ * @n: Inut int number of elements to display
+ * Return: Void.
+ */
+void print_array(int *a, int n)
+{
+	int l;
+
+	for (l = 0; l < n; l++)
+		printf("%d, ", a[l]);
+}
