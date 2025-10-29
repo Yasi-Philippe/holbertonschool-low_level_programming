30feb09253e33082e1fe5d757f6578ef22e439e3
Yasi-Philippe <shyasi95@gmail.com>
2025-10-29 21:37:54 +0100

    feat: C function that copies a string into another

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string into an adress with a maximum of a certain size.
+ * @dest: Destination string.
+ * @src: Source string.
+ * @n: Number of bytes to copy.
+ * Return: Void.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int c = 1;
+
+	while ((*dest++ = *src++))
+	{
+		if (c == n)
+			return (dest - n);
+		c++;
+	}
+	return (dest - c);
+}
