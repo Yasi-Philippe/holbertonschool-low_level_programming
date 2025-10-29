45c73dbb59b3c019d0a0be9438af8273e4c12ba1
Yasi-Philippe <shyasi95@gmail.com>
2025-10-29 10:27:50 +0100

    feat: C Function that prints half a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the last half of the character.
+ * @str: Input string.
+ *
+ * Return: Void.
+ */
+void puts_half(char *str)
+{
+	int l = 0;
+	int a;
+
+	while (str[l] != '\0')
+		l++;
+	for (a = (l / 2); a <= l; a++)
+		_putchar(str[a]);
+	_putchar('\n');
+}
