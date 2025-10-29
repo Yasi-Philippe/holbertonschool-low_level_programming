2bf62fa9cfcaca2f6a8830c854a3e0e71b2f64d4
Yasi-Philippe <shyasi95@gmail.com>
2025-10-29 11:42:04 +0100

    fix: Added function functionality for negative integers

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,14 @@ void print_array(int *a, int n)
 {
 	int l;
 
-	for (l = 0; l < n; l++)
-		if (l != n - 1)
-		{
-			printf("%d", a[l]);
-			printf(", ");
-		}
-		else
-			printf("%d\n", a[l]);
+	if (n > 0)
+		for (l = 0; l < n; l++)
+			if (l != n - 1)
+			{
+				printf("%d", a[l]);
+				printf(", ");
+			}
+			else
+				printf("%d\n", a[l]);
+	else{}
 }
