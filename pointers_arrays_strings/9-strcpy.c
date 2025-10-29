b9f6368a2c8187bd9b46601e7f5380146ac6fe0f
Yasi-Philippe <shyasi95@gmail.com>
2025-10-29 13:28:18 +0100

    feat: C function that copies another string

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies a string from a specified source to a specified destination.
+ * @dest: Input int array.
+ * @src: Inut int number of elements to display
+ * Return: Void.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+
+	while (src[l] != '\0')
+	{
+		dest[l] = src[l];
+		l++;
+	}
+	return (dest);
+}
