7ff76bc8c8fce9e023af56c696e28dafd1993fad
Yasi-Philippe <shyasi95@gmail.com>
2025-10-30 20:21:13 +0100

    feat: C function that makes each word's first letter uppercase

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - Turns all the first letters of a word into upper case.
+ * @str: Input string.
+ * Return: String with Uppercases.
+ */
+char *cap_string(char *str)
+{
+	char *ptr = str;
+	char sep[14] = {9, 10, 21, 32, 34, 40, 41, 44, 46, 59, 63, 123, 124};
+	int c = 0;
+	int mark = 0;
+
+	while (*ptr)
+	{
+		for (c = 0; c <= 12; c++)
+			if (*ptr == sep[c])
+			{
+				mark = 1;
+				break;
+			}
+		ptr++;
+		if (*ptr >= 97 && *ptr <= 122 && mark)
+			*ptr -= 32;
+		mark = 0;
+	}
+	return (str);
+}
