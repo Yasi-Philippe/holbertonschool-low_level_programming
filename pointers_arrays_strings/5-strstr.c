a5aca43bf3d870290429785804ee59e598ff0837
Yasi-Philippe <shyasi95@gmail.com>
2025-11-04 09:53:57 +0100

    feat: C Function that finds a substring in a string

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - Returns the first occurence of a matching substring in a string.
+ * @haystack: Input string.
+ * @needle: Input string to match.
+ *
+ * Return: Pointer to the beginning of the matching substring, or NULL.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int c;
+
+	while (*haystack)
+	{
+		if (*haystack == *needle)
+			for (c = 1;; c++)
+			{
+				if (haystack[c] == needle[c] && !needle[c + 1])
+					return (haystack);
+				if (haystack[c] != needle[c])
+					break;
+			}
+		haystack++;
+	}
+	return (0);
+}
