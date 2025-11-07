75f544dd288e86dc59758bde5051b8026904f8bb
Yasi-Philippe <shyasi95@gmail.com>
2025-11-07 15:54:53 +0100

    feat: C Function that prints the sum of numbers provided

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Prints the sum of two positive numbers.
+ * @argc: Lenght of the arguments of the command.
+ * @argv: Pointer to the command argument array.
+ * Return: Always 0 (Success!).
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0, c1, c2 = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+
+	for (c1 = 1; c1 < argc; c1++)
+	{
+		while (argv[c1][c2])
+		{
+			if (!isdigit(argv[c1][c2]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			c2++;
+		}
+		sum += atoi(argv[c1]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
