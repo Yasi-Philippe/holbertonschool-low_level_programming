69da649243f1c5c4daf05660734ad7e6c38ad639
Yasi-Philippe <shyasi95@gmail.com>
2025-11-07 23:25:22 +0100

    fix: Revamped code to include all integers up to INT MAX

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,4 +1,25 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * sqrt_check - Checks whether the square root of a number is a natural number
+ * @ptrn: Input integer.
+ * @ptr: Integer used to iterate the function.
+ * Return: The natural square number. -1 if this number does not exist.
+ */
+
+int sqrt_check(int *ptrn, int *ptr)
+{
+	int **ptr2 = &ptr;
+	int **ptrn2 = &ptrn;
+
+	if (**ptr2 * **ptr2 == **ptrn2)
+		return (**ptr2);
+	if (**ptr2 * **ptr2 > **ptrn2)
+		return (-1);
+	**ptr2 += 1;
+	return (sqrt_check(*ptrn2, *ptr2));
+}
 
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
@@ -6,16 +27,16 @@
  *
  * Return: Integer.
  */
+
 int _sqrt_recursion(int n)
 {
-	int a = 20000000;
+	int a = 1;
+	int *ptr = &a;
+	int *ptrn = &n;
+	int r;
 
 	if (n == 0)
 		return (0);
-	if (n < 0 || (n / a) * (n / a) > (n % a))
-		return (-1);
-	n += a;
-	if ((n / a) * (n / a) == (n % a))
-		return (n / a);
-	return (_sqrt_recursion(n));
+	r = sqrt_check(ptrn, ptr);
+	return (r);
 }
