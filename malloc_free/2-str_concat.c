deea92bd7b4d5ef8e5ebe733d1e4eb8fae9b31e0
Yasi-Philippe <shyasi95@gmail.com>
2025-11-11 00:46:22 +0100

    feat: C Function that concatenates two strings in the HEAP

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: Input string 1.
+ * @s2: Input string 2.
+ *
+ * Return: Pointer to the string concatenated in the HEAP. 0 if NULL.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int c1 = 0, c2 = 0;
+
+	while (s1[c1])
+		c1++;
+	while (s2[c2])
+		c2++;
+	s = malloc((c1 + c2 + 1) * sizeof(char));
+	if (s == NULL)
+		return (0);
+	c1--;
+	for (; c2 >= 0; c2--)
+		s[c1 + c2 + 1] = s2[c2];
+	for (; c1 >= 0; c1--)
+		s[c1] = s1[c1];
+	return (s);
+}
