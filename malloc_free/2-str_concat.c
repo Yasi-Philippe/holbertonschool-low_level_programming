837061c6eaad219c62e442b82b717b8286768d5f
Yasi-Philippe <shyasi95@gmail.com>
2025-11-11 14:59:54 +0100

    fix: Fixed bugs involving NULL outcome

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,16 +13,23 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	int c1 = 0, c2 = 0;
 
-	while (s1[c1])
-		c1++;
-	while (s2[c2])
-		c2++;
+	if (!s1 && !s2)
+		return (0);
+	if (s1)
+		while (s1[c1])
+			c1++;
+	if (s2)
+		while (s2[c2])
+			c2++;
 	s = malloc((c1 + c2 + 1) * sizeof(char));
 	if (s == NULL)
 		return (0);
 	c1--;
-	for (; c2 >= 0; c2--)
-		s[c1 + c2 + 1] = s2[c2];
+	if (s2)
+		for (; c2 >= 0; c2--)
+			s[c1 + c2 + 1] = s2[c2];
+	else
+		c1++;
 	for (; c1 >= 0; c1--)
 		s[c1] = s1[c1];
 	return (s);
