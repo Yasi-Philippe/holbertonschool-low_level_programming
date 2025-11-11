e508205eb19c1fb8263661b1cb4e13e808be391f
Yasi-Philippe <shyasi95@gmail.com>
2025-11-11 16:08:00 +0100

    fix: Freed memory leakage in the case of a fail

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -17,12 +17,18 @@ int **alloc_grid(int width, int height)
 		return (0);
 	ptr = malloc(height * sizeof(int *));
 	if (ptr == NULL)
+	{
+		free(ptr);
 		return (0);
+	}
 	for (r = 0; r < height; r++)
 	{
 		ptr[r] = malloc(width * sizeof(int));
 		if (ptr[r] == NULL)
+		{
+			free(ptr);
 			return (0);
+		}
 		for (c = 0; c < width; c++)
 			ptr[r][c] = 0;
 	}
