7963af32985888efb6280f6a8a0146a784d47177
Yasi-Philippe <shyasi95@gmail.com>
2025-11-11 16:03:16 +0100

    feat: C Function that allocates an array in  the HEAP

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - Prepares a bidimensional array in the HEAP.
+ * @width: Input width.
+ * @height: Input height.
+ *
+ * Return: Pointer to the bidimensional array. NULL if malloc fails.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int r, c;
+
+	ptr = malloc(height * sizeof(int *));
+	if (ptr == NULL)
+		return (0);
+	for (r = 0; r < height; r++)
+	{
+		ptr[r] = malloc(width * sizeof(int));
+		if (ptr[r] == NULL)
+			return (0);
+		for (c = 0; c < width; c++)
+			ptr[r][c] = 0;
+	}
+	return (ptr);
+}
