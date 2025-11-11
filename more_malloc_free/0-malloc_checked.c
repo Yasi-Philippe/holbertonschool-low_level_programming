3cd7843bf60010f563d9e7b2288e9c101133abcd
Yasi-Philippe <shyasi95@gmail.com>
2025-11-11 21:28:46 +0100

    feat: C Function that allocates memory

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates specified memory.
+ * @b: Pointer to the allocated memory.
+ *
+ * Return: - Pointer to allocated memory. 98 if unsuccessful.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(sizeof(b) * b);
+	if (!ptr)
+		return (0);
+	return (ptr);
+}
