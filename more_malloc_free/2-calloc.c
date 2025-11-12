55982f36473835f259f40459e808ef6e43b20c95
Yasi-Philippe <shyasi95@gmail.com>
2025-11-12 11:09:40 +0100

    feat: C Function calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - CAllocates memory and initializes it.
+ * @nmemb: Quantity of elements to allocate.
+ * @size: Size of each element.
+ *
+ * Return: - Pointer to allocated memory. Null if unseccessful.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+	unsigned int c;
+
+	if (!nmemb || !size)
+		return (0);
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (0);
+	for (c = 0; c <= (nmemb * size); c++)
+		ptr[c] = 0;
+	return (ptr);
+}
