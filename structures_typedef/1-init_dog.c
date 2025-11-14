d53b391ab3421ccb1064005d6d7f6660c7f01d62
Yasi-Philippe <shyasi95@gmail.com>
2025-11-14 15:14:48 +0100

    fix: Added condition for when the pointer given is Null

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * init_dog - Initializes a variable of the specified structure.
@@ -11,6 +12,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (!d)
+		exit(0);
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
