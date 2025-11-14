afab16588cd791eb281967914e76567f8461d51f
Yasi-Philippe <shyasi95@gmail.com>
2025-11-14 15:37:27 +0100

    fix: Added a 0 to the age value instead of Nil

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,9 +13,6 @@ void print_dog(struct dog *d)
 	if (!d)
 		return;
 	printf("Name: %s\n", (*d).name ? (*d).name : a);
-	if ((*d).age)
-		printf("Age: %f\n", (*d).age);
-	else
-		printf("Age: %s\n", a);
+	printf("Age: %f\n", (*d).age ? (*d).age : 0);
 	printf("Owner: %s\n", (*d).owner ? (*d).owner : a);
 }
