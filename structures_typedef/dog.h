42b100e6d413e91123010b0aee451d70e25b1ac8
Yasi-Philippe <shyasi95@gmail.com>
2025-11-14 14:09:36 +0100

    feat: C library with the structure for dog types

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure for a dog type.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Name of the owner of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
