fb96b8697b87380e79b1e6dc810ff3ee8a7479c7
Yasi-Philippe <shyasi95@gmail.com>
2025-11-19 08:37:49 +0100

    feat: C Function that prints all numbers passed into it

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - Prints all numbers provided.
+ * @separator: Separator to print between numbers
+ * @n: Quantity of integers to be printed.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int c;
+
+	va_start(args, n);
+	for (c = 0; c < n; c++)
+	{
+		printf("%d", va_arg(args, int));
+		if (n != c + 1)
+			printf("%s", separator);
+	}
+	putchar('\n');
+}
