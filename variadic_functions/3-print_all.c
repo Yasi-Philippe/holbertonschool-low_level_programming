73e6f18ea1195eee14a2f19b781b182e9365b596
Yasi-Philippe <shyasi95@gmail.com>
2025-11-21 13:13:32 +0100

    feat: C Function that prints any arguments passed into it

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,79 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_i - Prints an integer.
+ * @args: All the arguments given to the main function.
+ */
+void print_i(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - Prints a float.
+ * @args: All the arguments given to the main function.
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - Prints a string.
+ * @args: All the arguments given to the main function.
+ */
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (!str)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+ * print_char - Prints a char.
+ * @args: All the arguments given to the main function.
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_all - Prints all the arguments given to the function.
+ * @format: String with the keyword of all the argument types to be printed.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0, j = 0;
+	char *sep = "";
+	ty_pe t[] = {
+		{print_char, 'c'},
+		{print_i, 'i'},
+		{print_float, 'f'},
+		{print_string, 's'},
+	};
+
+	va_start(args, format);
+	while (format[i])
+	{
+		while (j <= 3)
+		{
+			if (t[j].key == format[i])
+			{
+				printf("%s", sep);
+				t[j].ptr(args);
+				sep = ", ";
+			}
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+	putchar('\n');
+	va_end(args);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,22 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct ty - Structure associating the keyword with the print fucntion to use
+ * @ptr: Function pointer that will point towards the printf function to use.
+ * @key: Keyword feining the type to print.
+ */
+typedef struct ty
+	{
+		void (*ptr)(va_list args);
+		char key;
+	} ty_pe;
 
 #endif
