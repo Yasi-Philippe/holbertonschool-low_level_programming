aeefd214ae4fa20d56333924b6b020a2f603157a
Yasi-Philippe <shyasi95@gmail.com>
2025-12-04 00:20:28 +0100

    feat: C Function that adds a node to the end of a list

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node - Adds a new node to the beginning of a list.
+ * @head: Pointer to the first node of the list.
+ * @str: String to store in the node.
+ * Return: size_t type pointer
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	list_t *actual_node;
+	int lgt = 0;
+
+	while (str[lgt])
+		lgt++;
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (0);
+	new_node->str = malloc(lgt + 1);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->str = strdup(str);
+	new_node->len = lgt;
+	while (actual_node->next)
+		actual_node = actual_node->next;
+	return (new_node);
+}
