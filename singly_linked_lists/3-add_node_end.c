7838e726d94812c802c45751577eff6c40322104
Yasi-Philippe <shyasi95@gmail.com>
2025-12-04 00:25:10 +0100

    fix: Fixed name of the function as well as desc

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 
 /**
- * add_node - Adds a new node to the beginning of a list.
+ * add_node_end - Adds a new node to the end of a list.
  * @head: Pointer to the first node of the list.
  * @str: String to store in the node.
  * Return: size_t type pointer
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
 	list_t *actual_node;
