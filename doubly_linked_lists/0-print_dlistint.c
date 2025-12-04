d6d07a0b8e41cdd5c469555b3a31e648b6b34a1f
Yasi-Philippe <shyasi95@gmail.com>
2025-12-04 15:56:31 +0100

    feat: C Function that prints the elements of a list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * print_dlistint - Counts the number of nodes in a list.
+ * @h: Pointer to the first node of the list.
+ * Return: size_t type , number of nodes.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t lg = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		lg++;
+	}
+	return (lg);
+}
