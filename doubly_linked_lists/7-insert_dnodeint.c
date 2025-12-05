71938534377f255220aadf2755811f0620796b22
Yasi-Philippe <shyasi95@gmail.com>
2025-12-05 09:53:40 +0100

    fix: Added a case when index is zero

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -24,6 +24,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (0);
+	if (!idx)
+	{
+		new_node->n = n;
+		*h = new_node;
+		return (new_node);
+	}
 	new_node->n = n;
 	new_node->prev = actual_node->prev;
 	new_node->next = actual_node;
