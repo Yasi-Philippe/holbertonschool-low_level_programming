78b1fdd88ed8fe0d67f8b5b2e81e8e8a04f82d7b
Yasi-Philippe <shyasi95@gmail.com>
2025-12-05 19:38:27 +0100

    fix: Returns 1 when an empty file is created

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,6 +17,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+		return (1);
 	while (text_content[len])
 		len++;
 	w = write(fd, text_content, len);
