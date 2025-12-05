9e38a00a5f245480ae3431a033406e67e454e4f0
Yasi-Philippe <shyasi95@gmail.com>
2025-12-05 17:56:43 +0100

    feat: C Function that writes text from a file

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * read_textfile - Writes a text from a file in the standard output.
+ * @filename: Name of the file
+ * @letters: AMount of letters to print.
+ * Return: ssize_t type, letters printed. 0 if it fails.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    size_t file1;
+	ssize_t len;
+	char buffer[4096];
+
+	if (!filename)
+		return (0);
+	file1 = open(filename, O_RDWR);
+	if (!file1)
+		return (0);
+	read(file1, buffer, sizeof(buffer));
+	len = write(file1, buffer, letters);
+	if (!len)
+		return (0);
+	return (len);
+}
---
+++ file_io/main.h
@@ -0,0 +1,9 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <sys/types.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
