01794747bd28fc8071cee47477a330ab6a81f1f7
Yasi-Philippe <shyasi95@gmail.com>
2025-12-05 19:31:24 +0100

    feat: C Function that creates a file and writes text in it

---
+++ file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_file - Creates a file.
+ * @filename: Name of the file
+ * @text_content: Content in the file.
+ * Return: 1 on success. -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fd, w;
+	size_t len = 0;
+
+	if (!filename || !text_content)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	while (text_content[len])
+		len++;
+	w = write(fd, text_content, len);
+	close(fd);
+	if (w == -1)
+		return (-1);
+	return (1);
+}
